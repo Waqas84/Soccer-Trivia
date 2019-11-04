9e5a98222d3b65ea925334a7d68a02cd3755ce58
Waqas Alsubayee <walsubayee@yahoo.com>
2019-11-04 12:57:01 -0800

    init

---
+++ sketch/about.md
@@ -0,0 +1,24 @@
+# Soccer Trivia
+## Questions:
+1. Who won the 1994 FIFA World Cup?
+   Options: "Italy", "Argentina", "India" "Brazil".
+   Correct answer: "Brazil"
+
+2. According to the official FIFA rulebook, how long can         a goalkeeper hold onto the ball for?"
+   Options: "3 Seconds", "5 Seconds", "30 Seconds", "10 Seconds".
+      Correct answer: "5 Seconds"
+
+3. Anfield is the home of which English Premier League club?",
+   Options: "Man United", "Man City", "Liverpool", "Arsenal"
+   Correct answer: "Liverpool"
+
+4. Why did the Indian national team withdraw from the FIFA       World Cup competition in 1950?"
+   Options: "As a political protest.", "Because they could not play barefoot.",
+   "They didn't, that's just a soccer myth."
+   Correct answer: "Because they could not play barefoot"
+
+5. In the MLS in what city does the team Chivas USA play?",
+  Options: "Buffalo, New York", "Carson, California", "Miami, Florida", "San Diego, California"
+  Correct answer: "Carson, California".
+
+  
---
+++ store.js
@@ -0,0 +1,47 @@
+const STORE = {
+  questions: [
+    {
+      question: "Who won the 1994 FIFA World Cup?",
+      options: ["Italy", "Argentina", "India", "Brazil"],
+      answer: "Brazil"
+    },
+    {
+      question:
+        "According to the official FIFA rulebook, how long can a goalkeeper hold onto the ball for?",
+      options: ["3 Seconds", "5 Seconds", "30 Seconds", "10 Seconds"],
+      answer: "5 Seconds"
+    },
+    {
+      question: "Anfield is the home of which English Premier League club?",
+      options: ["Man United", "Man City", "Liverpool", "Arsenal"],
+      answer: "Liverpool"
+    },
+    {
+      question:
+        "Why did the Indian national team withdraw from the FIFA World Cup competition in 1950?",
+      options: [
+        "As a political protest.",
+        "Because they could not play barefoot.",
+        "They didn't, that's just a soccer myth."
+      ],
+      answer: "Because they could not play barefoot"
+    },
+    {
+      question: "In the MLS in what city does the team Chivas USA play?",
+      options: [
+        "Buffalo, New York",
+        "Carson, California",
+        "Miami, Florida",
+        "San Diego, California"
+      ],
+      answer: "<Carson, California"
+    },
+    {
+      question: "What are the home colors of the FC Barcelona soccer uniform?",
+      options: ["Black & White", "Blue & Red", "Orange & Yellow", "Blue"],
+      answer: "Blue & Red"
+    }
+  ],
+  currentQuestion: 0,
+  score: 0
+};
